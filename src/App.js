a50ec1b51359e7b9799d8118d93200caa43cf732
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-08-29 08:08:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+
+const initialState = {
+  questions: [],
+  status: 'loading'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {...state, questions: action.payload, status: 'ready'}
+    case 'dataFailed':
+      return {...state, status: 'error'}
+    default:
+      throw new Error('The action is unknown')
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({type: 'dataReceived', payload: data}))
+      .catch(err => dispatch({type: 'dataFailed'}))
+  }, []);
   return (
-    <div>
-      <DateCounter/>
+    <div className='app'>
+      <Header/>
+      <Main>
+        <p></p>
+        <p>Questions</p>
+      </Main>
     </div>
   )
 }
